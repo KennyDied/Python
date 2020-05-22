67a3d0cda5da8d66946f2c497b54c0d535027a9f
KennyDied <thekennydied@mail.ru>
2020-05-22 17:13:21 +0300

    Домашнее задание по Алгебре и геометрии

---
+++ AlGemTaskCSF/Bykovsky_Task/main.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+from math import sin, cos, pi
+
+plt.title('Асторида')
+plt.xlabel('Ось x')
+plt.ylabel('Ось y')
+plt.grid()
+
+R = 1000
+n = range(1000)
+t = [2 * pi * i / 100 for i in n]
+x = [R * cos(t[i]) ** 3 for i in n]
+y = [R * sin(t[i]) ** 3 for i in n]
+
+plt.plot(x, y, 'r')
+plt.show()
