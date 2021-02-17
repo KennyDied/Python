1863e5962e095b78cc3cf6271d917c9ed832ab43
KennyDied <thekennydied@mail.ru>
2021-02-17 21:24:39 +0300

    add 1 task

--- CSF_Tasks/Task1/Task1.py
+++ CSF_Tasks/Task1/Task1.py
@@ -1,16 +1,22 @@
-def solve_task(lst, n):
+def read_data():
+    return open('inputFile.txt').read().split(',')
+
+
+def solve_task(my_list, n):
     if n < 0:
         n = abs(n)
         for i in range(n):
-            lst.append(lst.pop(0))
+            my_list.append(my_list.pop(0))
     else:
         for i in range(n):
-            lst.insert(0, lst.pop())
+            my_list.insert(0, my_list.pop())
+    return my_list
 
-first_list = [3, 5, 2, 4, 9, 1]
-solve_task(first_list, 2)
-print(first_list)
 
-second_list = [3, 5, 2, 4, 9, 1]
-solve_task(second_list, - 15)
-print(second_list)
+def write_data(my_new_list):
+    with open('outputFile.txt', 'w') as f:
+        for line in my_new_list:
+            f.write(line + ',')
+
+
+write_data(solve_task(read_data(), 2))
